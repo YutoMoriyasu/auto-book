c18445793b63b8b27728068014e1684f5710d378
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-01-14 22:58:33 +0900

    ログイン機能実装

--- app/__init__.py
+++ app/__init__.py
@@ -1,119 +1,36 @@
-import os
-
-from flask import Flask, render_template, request, redirect
-from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import *
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///user.db')  # user.db というデータベースを使うという宣言です
-Base = declarative_base()  # データベースのテーブルの親です
+db = SQLAlchemy()
 
-# usersテーブル定義
-class User(Base):  # PythonではUserというクラスのインスタンスとしてデータを扱います
-  __tablename__ = 'users'  # テーブル名は users です
-  user_id = Column(Integer, primary_key=True, unique=True)  # 整数型のid をprimary_key として、被らないようにします
-  email = Column(String)  # 文字列の emailというデータを作ります
-  name = Column(String)  # 文字列の nameというデータを使います
-  def __repr__(self):
-      return "User<{}, {}, {}>".format(self.user_id, self.email, self.name)
-
-# postsテーブル定義
-
-# groupsテーブル定義
-
-
-Base.metadata.create_all(engine)  # 実際にデータベースを構築します
-SessionMaker = sessionmaker(bind=engine)  # Pythonとデータベースの経路です
-session = SessionMaker()  # 経路を実際に作成しました
-
-def create_app(test_config=None):
+def create_app():
   # create and configure the app
   app = Flask(__name__, instance_relative_config=True)
-  app.config.from_mapping(
-    SECRET_KEY='dev',
-  )
+  app.config['SECRET_KEY'] = 'secret-key-goes-here'
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = false
+
+  db.init_app(app)
 
   login_manager = LoginManager()
+  login_manager.login_view = 'auth.login'
   login_manager.init_app(app)
+
+  from .models import User
 
   @login_manager.user_loader
   def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
     return User.query.get(int(user_id))
-  
-  if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-  else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
 
-  # ensure the instance folder exists
-  try:
-    os.makedirs(app.instance_path)
-  except OSError:
-    pass
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint)
 
-  # トップページ
-  @app.route('/')
-  @login_required
-  def index():
-    # TODO ユーザーに対して、登録しているグループの情報を取得する処理を記述する
-    test = 'テンプレートエンジンのテスト'
-    return render_template('index.html', test=test)
-
-  # 個別のグループページ
-  @app.route('/groups/<group_id>') # /groups/<group_id>にアクセスしたとき
-  @login_required
-  def group(group_id):
-    # TODO 個別のグループの情報を取得する処理を記述する
-    return render_template('group.html', group_id=group_id) # group.htmlに変数group_idを渡す
-  
-  # サインアップページ
-  @app.route('/signup', methods=['GET','POST'])
-  def signup():
-    if request.method == 'POST':
-      username = request.form.get('username')
-      email = request.form.get('email')
-      # Userのインスタンスを作成
-      user = User(name = username, email = email)
-      session.add(user)
-      session.commit()
-      return redirect('/login')
-    else:
-      return render_template('signup.html')
-
-  # ログイン画面
-  @app.route('/login', methods=['GET','POST'])
-  def login():
-    if request.method == 'POST':
-      username = request.form.get('username')
-      email = request.form.get('email') 
-      user = session.query(User).filter(User.name == username).all()
-      for user in user:
-        if user.email == email:
-          return render_template('index.html')
-    else:
-      return render_template('login.html')
-  
-  # ログアウト
-  @app.route('/logout')
-  @login_required
-  def logout():
-    logout_user()
-    return redirect('/login')
-  
-  # 記事登録画面
-  @app.route('/register')
-  @login_required
-  def register():
-    return render_template('register.html')
-  
-  # 記事の表示
-  @app.route('/all')
-  @login_required
-  def all():
-    return render_template('all.html', all='all')
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
 
   return app
